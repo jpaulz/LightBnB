d733f218b5c7c0036e905749c4546ce98ebf41cf
Julia Paulasaari <julia@paulasaari.com>
2019-07-25 03:02:18 +0000

    typed in data into tables

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO users (id, name, email, password)
+	-- VALUES (1, 'Malloy Jenkins', 'mall@jen.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+  VALUES
+(5, 'Eva Stanley','eva@sta.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+(6, 'Louisa Meyer','lou@mey.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO properties (
+title, 
+description, 
+thumbnail_photo_url,
+cover_photo_url,
+cost_per_night,
+parking_space,
+number_of_bathrooms,
+number_of_bedrooms,
+country,
+street,
+city,
+province,
+post_code,
+active,
+owner_id)
+VALUES ('speed lamp', 'description', 'http;//jdhfjdj', 'http;//images.kjkjk', 930, 6, 4, 8, 'Canada', '536 Ham street', 'Sotkbe', 'Quebec', 23485, 'true', 1),
+('blank corner', 'description', 'http;//jdhfjdj', 'http;//images.kjkjk', 670, 7, 5, 6, 'Canada', '636 Ham street', 'Sotkbe', 'Quebec', 23485, 'true', 5);
+
